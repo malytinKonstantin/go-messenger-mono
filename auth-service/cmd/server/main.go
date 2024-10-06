69c5ca5bbbc7a9ac0c8f6446a5f556a191a437e0
malytinKonstantin <const.malytin@gmail.com>
2024-10-06 21:34:00 +0700

    🏗 Refactor auth service to include database integration and modularize components
    - Enhanced `.env` to include database and Kafka configurations.
    - Moved main application entry point to `cmd/server/` for better organization.
    - Implemented database schema and query files for user authentication and OAuth.
    - Updated `docker-compose.yml` to leverage `.env` for environment configuration.
    - Added SQLc for database query management and code generation.
    - Created modular structure with repositories, services, and handlers for authentication processes.
    - Enhanced Go module dependencies to support new database operations and configuration management using Viper.

--- auth-service/cmd/server/main.go
+++ auth-service/cmd/server/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"log"
 	"net"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
 	pb "github.com/malytinKonstantin/go-messenger-mono/proto/auth-service"
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
 
@@ -22,15 +26,33 @@ func (s *server) Authenticate(ctx context.Context, in *pb.AuthRequest) (*pb.Auth
 }
 
 func main() {
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092"})
+	viper.SetConfigFile(".env")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		viper.GetString("DATABASE_HOST"),
+		viper.GetString("DATABASE_PORT"),
+		viper.GetString("DATABASE_USER"),
+		viper.GetString("DATABASE_PASSWORD"),
+		viper.GetString("DATABASE_NAME"))
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": viper.GetString("KAFKA_BOOTSTRAP_SERVERS")})
 	if err != nil {
 		log.Fatalf("Failed to create producer: %s", err)
 	}
 	defer p.Close()
 
-	lis, err := net.Listen("tcp", ":50052")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", viper.GetString("GRPC_PORT")))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterAuthServiceServer(s, &server{producer: p})
@@ -42,11 +64,11 @@ func main() {
 	})
 
 	go func() {
-		log.Fatal(app.Listen(":3002"))
+		log.Fatal(app.Listen(fmt.Sprintf(":%s", viper.GetString("HTTP_PORT"))))
 	}()
 
 	log.Printf("gRPC server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("Failed to serve: %v", err)
 	}
 }
